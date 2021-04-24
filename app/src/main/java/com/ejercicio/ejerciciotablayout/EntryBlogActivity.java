c0844e7daf70ea72819507029aaca91ea1d4248f
Javier Catalán <74968085+javiercatalancas@users.noreply.github.com>
2021-04-24 12:52:47 +0200

    24042021
    Arreglar las vistas y seguir con el boton2

--- app/src/main/java/com/ejercicio/ejerciciotablayout/EntryBlogActivity.java
+++ app/src/main/java/com/ejercicio/ejerciciotablayout/EntryBlogActivity.java
@@ -1,9 +1,12 @@
 package com.ejercicio.ejerciciotablayout;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.ejercicio.ejerciciotablayout.usingclasses.Pelicula;
 
 public class EntryBlogActivity extends AppCompatActivity {
 
@@ -11,5 +14,22 @@ public class EntryBlogActivity extends AppCompatActivity {
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.entrypeli_layout);
+
+        Bundle bundle = getIntent().getBundleExtra("extra");
+        Pelicula peli = bundle.getParcelable("objeto");
+
+        ImageView imgheader = findViewById(R.id.imageheader);
+        TextView title = findViewById(R.id.textitle);
+        TextView text3 = findViewById(R.id.textdesc);
+        TextView text4 = findViewById(R.id.textfecha);
+
+        //setTitle(peli.getTitulo());
+        title.setText(peli.getTitulo());
+        text3.setText(peli.getDescripcion());
+        text4.setText(peli.getFecha());
+
+        int url = getResources().getIdentifier(peli.getImagencab(), "drawable", getPackageName());
+        imgheader.setImageResource(url);
+
     }
 }
